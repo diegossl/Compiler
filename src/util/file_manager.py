3f64b9570f79cfd6f90d8d45a792311c05a0ea5e
Diego <diegossl94@gmail.com>
2021-03-07 16:54:20 -0300

    feat: writeFile method added

--- src/util/file_manager.py
+++ src/util/file_manager.py
@@ -10,15 +10,17 @@ class FileManager:
 
     for fileName in listdir(filePath):
       if isfile(join(filePath, fileName)):
-        with open(join(filePath, fileName), 'r') as content:
-          text = content.read()
-          fileData.append({
-            'fileName': fileName,
-            'content': text
-          })
+        with open(join(filePath, fileName), 'r') as file:
+          text = file.read()
+          fileData.append({ 'content': text })
           
     return fileData
 
   @staticmethod
-  def writeFiles(data):
-    pass
+  def writeFile(fileNumber, data):
+    filePath = 'output'
+
+    with open(join(filePath, f'saida{fileNumber + 1}.txt'), 'w+') as file:
+      file.write('Token List\n\n')
+      for token in data:
+        file.write(f'{token.tokenLine} {token.tokenType} {token.tokenValue}\n')
