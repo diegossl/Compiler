87581492db0b924b2454b12cd49b06c94050d9d5
Diego <diegossl94@gmail.com>
2021-03-07 16:55:43 -0300

    refactor: everything has been changed

--- src/lexical_analyzer/scanner.py
+++ src/lexical_analyzer/scanner.py
@@ -1,44 +1,38 @@
-from .definition.types import (ARI_TK, BLO_TK, DEL_TK, DIG_TK, IDE_TK, LET_TK, LIN_TK, LOG_TK,
-  NUN_TK, REL_TK, RES_TK, STR_TK)
-from .comparator import Comparator
-import re
+from .constants.types import NRO_TK, IDE_TK, PRE_TK, BCM_TK, LCM_TK, NWL_TK, SKP_TK
+from .constants.lexical import RESERVED_WORDS
+from .constants.map import TOKENS_MAP
+from .Token import Token
+import re as regex
 
 class Scanner:
 
-  def __init__(self):
-    self.comparator = Comparator()
-    self.content = None
-    self.position = 0
-    self.term = None
-    self.state = 0
- 
-  def getToken(self):
-    if self.__isEndFile():
-      return None
+  def tokenize(self, code):
+    superRegex = '|'.join('(?P<%s>%s)' % pair for pair in TOKENS_MAP)
+    lineNum = 1
+    lineStart = 0
 
-    self.state = 0
+    for matches in regex.finditer(superRegex, code):
+      groupType = matches.lastgroup
+      value = matches.group()
       
-    while True:
-      currentChar = self.content[self.position]
+      if groupType == NRO_TK:
+        value = float(value) if '.' in value else int(value)
+      if groupType == IDE_TK and value in RESERVED_WORDS:
+        groupType = PRE_TK
+      if groupType == BCM_TK:
+        count = 0
+        for char in value:
+          if char == '\n':
+            count += 1
+        lineNum += count
+      if groupType == NWL_TK:
+        lineStart = matches.end()
+        lineNum += 1
+        continue
+      if groupType == SKP_TK:
+        continue
+      if groupType == 'MISMATCH':
+        raise RuntimeError(f'{value!r} unexpected on line {lineNum}')
 
-      if self.state == 0:
-        if self.comparator.isDelimiter(currentChar):
-          self.term = currentChar
-          self.state = 25
-      elif self.state == 25:
-        return { 'type': DEL_TK, 'term': self.term }
-      else:
-        return { 'type': None, 'term': None }
-
-      self.position += 1
-
-  def __currentChar(self):
-    currentChar = self.content[self.position]
-    self.position += 1
-    return currentChar
-
-  def __isEndFile(self):
-    return self.position == len(self.content) - 1
-  
-  def __back(self):
-    self.position -= 1
+      if groupType != BCM_TK and groupType != LCM_TK:
+        yield Token(groupType, value, lineNum)
