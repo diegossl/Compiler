66905f991cd504c1f91e90d24702025cb5fa3013
Diego <diegossl94@gmail.com>
2021-03-09 21:04:52 -0300

    refactor: writeFiles method has been changed

--- src/util/file_manager.py
+++ src/util/file_manager.py
@@ -7,20 +7,20 @@ class FileManager:
   def readFiles():
     filePath = 'input'
     fileData = []
-
     for fileName in listdir(filePath):
       if isfile(join(filePath, fileName)):
         with open(join(filePath, fileName), 'r') as file:
           text = file.read()
           fileData.append({ 'content': text })
-          
     return fileData
 
   @staticmethod
-  def writeFile(fileNumber, data):
+  def writeFile(fileNumber, tokens, errors):
     filePath = 'output'
-
     with open(join(filePath, f'saida{fileNumber + 1}.txt'), 'w+') as file:
-      file.write('Token List\n\n')
-      for token in data:
+      file.write('List of tokens\n\n')
+      for token in tokens:
         file.write(f'{token.tokenLine} {token.tokenType} {token.tokenValue}\n')
+      file.write('\nList of errors\n\n')
+      for error in errors:
+        file.write(f'{error.tokenLine} {error.tokenType} {error.tokenValue}\n')
