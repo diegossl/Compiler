9f08a58a48579fba607ef3632b2fb54bb444158a
Diego <diegossl94@gmail.com>
2021-03-16 16:38:07 -0300

    refactor: output has been changed

--- src/util/file_manager.py
+++ src/util/file_manager.py
@@ -18,18 +18,18 @@ class FileManager:
   def writeFile(fileNumber, tokens, errors):
     filePath = 'output'
     with open(join(filePath, f'saida{fileNumber + 1}.txt'), 'w+') as file:
-      file.write('List (lines, tokens, lexemes):\n\n')
+      file.write('List of tokens (lines, tokens, lexemes):\n\n')
       firstColumnTabs = None
       secondColumnTabs = None
       for token in tokens:
         firstColumnTabs = '\t\t' if token.tokenLine <= 9 else '\t'
         secondColumnTabs = '\t'
         file.write(f'{token.tokenLine}{firstColumnTabs}{token.tokenType}{secondColumnTabs}{token.tokenValue}\n')
+      file.write('\nList of errors (lines, tokens, lexical errors):\n\n')
       if len(errors) > 0:
-        file.write('\nList (lines, tokens, lexical errors):\n\n')
         for error in errors:
           firstColumnTabs = '\t\t' if error.tokenLine <= 9 else '\t'
           secondColumnTabs = '\t\t' if len(error.tokenType) < 4 else '\t'
           file.write(f'{error.tokenLine}{firstColumnTabs}{error.tokenType}{secondColumnTabs}{error.tokenValue}\n')
       else:
-        file.write('\nNo lexical errors')
+        file.write('No lexical errors')
