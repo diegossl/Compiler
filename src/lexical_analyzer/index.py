6070691fdae77041c4f3ca25020d541b35721f6b
Diego <diegossl94@gmail.com>
2021-03-16 10:26:46 -0300

    refactor: return all tokens added

--- src/lexical_analyzer/index.py
+++ src/lexical_analyzer/index.py
@@ -7,18 +7,26 @@ class LexicalAnalyzer:
     self.scanner = Scanner()
   
   def startAnalysis(self):
+    tokensByFile = []
+
     for fileNumber, file in enumerate(FileManager.readFiles()):
       self.scanner.code = file['content']
       fileName = file['fileName']
       print(f'INPUT: {fileName}')
       tokens = []
       errors = []
+
       for error in self.scanner.scanErrors():
         errors.append(error)
       for token in self.scanner.scanTokens():
         tokens.append(token)
+
+      tokensByFile.append({ 'file': fileName, 'tokens': tokens })
       FileManager.writeFile(fileNumber, tokens, errors)
+
       if len(errors) > 0:
         print(f'OUTPUT: {len(errors)} lexical errors were found\n')
       else:
         print(f'OUTPUT: Successfully completed! {len(tokens)} leximes were found\n')
+        
+    return tokensByFile
